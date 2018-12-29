c6e356fddb0ad4cade2653a907505e1c3c67ec3b
Rolf Bansbach <rolfisub@gmail.com>
2018-12-29 15:41:42 -0500

    enough unittests for today

--- tests/PrintKeyValueTest.php
+++ tests/PrintKeyValueTest.php
@@ -1,7 +1,9 @@
 <?php
 require "./src/PrintKeyValue.php";
+
 use PHPUnit\Framework\TestCase;
 use Rolfisub\PrintKeyValue\PrintKeyValue;
+
 /**
  *  Corresponding Class to test YourClass class
  *
@@ -30,10 +32,67 @@ class PrintKeyValueTest extends TestCase
     /**
      * testing that the return type of the method is string
      */
-    public function testIsString() {
-        $obj = new PrintKeyValue("","  ");
-        $out = $obj->printKeyValues([[1,[2,[4]],3]]);
+    public function testIsString()
+    {
+        $obj = new PrintKeyValue("", "  ");
+        $out = $obj->printKeyValues([[1, [2, [4]], 3]]);
         $this->assertTrue(is_string($out));
+    }
+
+    /**
+     * tests that after any given array structure the expected output is correct
+     */
+    public function testIsStructureCorrect()
+    {
+        //test 1
+        $kvp = new PrintKeyValue();
+        $a1 = [1];
+        $ex = "0:1\n";
+        $kvp->clear();
+        $res1 = $kvp->printKeyValues($a1);
+        $this->assertContains($ex, $res1);
+
+        //test 2
+        $a2 = [1, 2];
+        $ex2 = "0:1\n1:2\n";
+        $kvp->clear();
+        $res2 = $kvp->printKeyValues($a2);
+        $this->assertContains($ex2, $res2);
+
+        //test 3
+        $a3 = [1, [2]];
+        $ex3 = "0:1\n{\n 0:2\n}\n";
+        $kvp->clear();
+        $res3 = $kvp->printKeyValues($a3);
+        $this->assertContains($ex3, $res3);
+
+
+        //test 4
+        $a4 = [1, 2, [3, [4]]];
+        $ex4 = "0:1\n1:2\n{\n 0:3\n {\n  0:4\n }\n}\n";
+        $kvp->clear();
+        $res4 = $kvp->printKeyValues($a4);
+        $this->assertContains($ex4, $res4);
+
+        //test5
+        $a5 = [
+            "key" => "value",
+            5,
+            [
+                "another" => "keyvalue",
+                [
+                    5,
+                    [
+                        "yes" => "no"
+                    ]
+                ]
+            ]
+        ];
+        $ex5 = "key:value\n0:5\n{\n another:keyvalue\n {\n  0:5\n  {\n   yes:no\n  }\n }\n}\n";
+        $kvp->clear();
+        $res5 = $kvp->printKeyValues($a5);
+        $this->assertContains($ex5, $res5);
+
     }
 
 }
